02048347d77d72a9ad1a047e5430096f682ccc06
jsxgod <smykoo7@gmail.com>
2022-02-06 23:27:24 +0100

    create HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LogoAnimation } from "../components";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <LogoAnimation />
+    </div>
+  );
+};
+
+export default HomePage;
