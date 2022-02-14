2715166172c49b557fe7490abc18c84b4568d735
jsxgod <smykoo7@gmail.com>
2022-02-14 03:45:52 +0100

    replace keyframes animation with framer-motion animation

--- src/components/LogoAnimation.jsx
+++ src/components/LogoAnimation.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { ReactComponent as Logo } from "../assets/logo_outline.svg";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 1,
+      ease: [0.6, 0.05, -0.01, 0.99],
+    },
+  },
+  hide: {
+    opacity: 0,
+    transition: { duration: 0.5, ease: [0.6, 0.05, -0.01, 0.99] },
+  },
+};
 
 const LogoAnimation = () => {
   return (
-    <div className="logo-animation">
+    <motion.div
+      variants={variants}
+      initial="hidden"
+      animate="show"
+      exit="hide"
+      className="logo-animation"
+    >
       <div className="logo-wrapper">
         <Logo className="logo" />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
