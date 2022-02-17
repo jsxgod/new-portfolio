af7b6b940ada470b12a364cd986e7f6fd7344f79
jsxgod <smykoo7@gmail.com>
2022-02-17 14:24:08 +0100

    LogoAnimation moved here. Add custom cursor.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { useState, useEffect, useRef } from "react";
 import { HomePage } from "./pages";
+import { LogoAnimation } from "./components";
+import { AnimatePresence } from "framer-motion";
 
 import "./sass/main.scss";
 
 const App = () => {
+  const [showAnimation, setShowAnimation] = useState(true);
+  const customCursor = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAnimation(false);
+    }, 2000);
+  }, []);
+
+  const moveCursor = (event) => {
+    try {
+      customCursor.current.style.left = `${event.clientX}px`;
+      customCursor.current.style.top = `${event.clientY}px`;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <HomePage />
+    <div className="App" onMouseMove={(event) => moveCursor(event)}>
+      <div className="custom-cursor" ref={customCursor}></div>
+      <AnimatePresence exitBeforeEnter>
+        {showAnimation ? (
+          <LogoAnimation key="logo" />
+        ) : (
+          <HomePage key="homepage" />
+        )}
+      </AnimatePresence>
     </div>
   );
 };
