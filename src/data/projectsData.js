9746cca6ad2e79dcfb19ae2819fc27f430a6649f
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-05 17:42:11 +0200

    Mock data for the projects section.

---
+++ src/data/projectsData.js
@@ -0,0 +1,32 @@
+export const projectsData = [
+  {
+    name: "project name 1",
+    thumbnail:
+      "https://gainbitcoin.com/wp-content/uploads/2020/04/Automatic-Fortunes-Investing-in-the-Number-One-Tech-Stock-696x449.jpg",
+  },
+  {
+    name: "project name 2",
+    thumbnail:
+      "https://gainbitcoin.com/wp-content/uploads/2020/04/Automatic-Fortunes-Investing-in-the-Number-One-Tech-Stock-696x449.jpg",
+  },
+  {
+    name: "project name 3",
+    thumbnail:
+      "https://gainbitcoin.com/wp-content/uploads/2020/04/Automatic-Fortunes-Investing-in-the-Number-One-Tech-Stock-696x449.jpg",
+  },
+  {
+    name: "project name 4",
+    thumbnail:
+      "https://gainbitcoin.com/wp-content/uploads/2020/04/Automatic-Fortunes-Investing-in-the-Number-One-Tech-Stock-696x449.jpg",
+  },
+  {
+    name: "project name 3",
+    thumbnail:
+      "https://gainbitcoin.com/wp-content/uploads/2020/04/Automatic-Fortunes-Investing-in-the-Number-One-Tech-Stock-696x449.jpg",
+  },
+  {
+    name: "project name 4",
+    thumbnail:
+      "https://gainbitcoin.com/wp-content/uploads/2020/04/Automatic-Fortunes-Investing-in-the-Number-One-Tech-Stock-696x449.jpg",
+  },
+];
