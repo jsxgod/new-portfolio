39882faea6bfaa6d94350f4eb9604ce48bea50b1
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-05 12:54:14 +0200

    Create and animate the about section

---
+++ src/components/About.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { useEffect } from "react/cjs/react.production.min";
+
+const cardOffsets = [5, 0, -5];
+
+const About = () => {
+  const [technologies, setTechnologies] = useState([
+    { name: "html", icon: require("../assets/html_icon.png") },
+    { name: "javascript", icon: require("../assets/js_icon.png") },
+    { name: "react", icon: require("../assets/react_icon.png") },
+    { name: "css", icon: require("../assets/css_icon.png") },
+    { name: "sass", icon: require("../assets/sass_icon.png") },
+  ]);
+
+  const [hideIcon, setHideIcon] = useState(false);
+  const [blockScroll, setBlockScroll] = useState(false);
+
+  const handleWheel = (e) => {
+    // scroll down
+    if (!blockScroll) {
+      if (e.deltaY < 0) {
+        setTechnologies((technologies) =>
+          technologies.slice(1).concat(technologies.slice(0, 1))
+        );
+      } // scroll up
+      else if (e.deltaY > 0) {
+        setTechnologies((technologies) =>
+          technologies.slice(-1).concat(technologies.slice(0, -1))
+        );
+      }
+      setHideIcon(true);
+      setBlockScroll(true);
+      setTimeout(() => {
+        setHideIcon(false);
+        setTimeout(() => {
+          setBlockScroll(false);
+        }, 100);
+      }, 100);
+    }
+  };
+
+  return (
+    <div className="section">
+      <div className="about-me-wrapper">
+        <div className="info-wrapper text">
+          <div className="text-wrapper">
+            <h2>About me</h2>
+            <p>
+              I'm a web developer – learn more about me and my interests. I'm
+              available for web projects and based in Wrocław, Poland. Feel free
+              to get in touch at the bottom of the page if you're interested in
+              working together.
+            </p>
+          </div>
+          <div className="text-wrapper">
+            <h2>Education</h2>
+            <p>
+              I studied computer science at the Wrocław University of Science
+              and Technology. There I have learned about topics ranging from
+              pure mathematics (algebra, calculus, discrete mathematics, logic,
+              statistics) through various low and high level programming
+              languages (C, C++, Java, Javascript, Python, GO, Julia, Prolog,
+              Assembly) as well as databases, data analysis or even compiler
+              design and implementation.
+            </p>
+          </div>
+        </div>
+        <div className="info-wrapper technology">
+          <h2>Technological stack</h2>
+          <div
+            className="technology-showcase-wrapper"
+            onMouseEnter={() => {
+              document.querySelector(".custom-cursor").classList.add("scroll");
+              document.querySelector("body").classList.add("no-scroll");
+            }}
+            onMouseLeave={() => {
+              document
+                .querySelector(".custom-cursor")
+                .classList.remove("scroll");
+              document.querySelector("body").classList.remove("no-scroll");
+            }}
+            onWheel={(e) => handleWheel(e)}
+          >
+            {cardOffsets.map((offset, i) => (
+              <motion.div
+                key={i}
+                className="card"
+                style={{ top: `${50 + offset}%`, zIndex: { i } }}
+              >
+                <motion.img
+                  src={technologies[i].icon}
+                  alt={technologies[i].name}
+                  animate={hideIcon ? { opacity: 0 } : { opacity: 1 }}
+                />
+              </motion.div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
