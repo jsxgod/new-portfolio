c6639bd9b84a5f1a3f9287d9a364baa5ed36e45e
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-05 21:00:37 +0200

    Animate the projects section thumbnail pop up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+
 import { projectsData } from "../data/projectsData";
+
+const hoverTextOffset = -50;
+
+const textLeftVariants = {
+  rest: {
+    x: 0,
+    transition: { type: "spring", duration: 0.5, stiffness: 100 },
+  },
+  hover: {
+    x: hoverTextOffset,
+    zIndex: 3,
+    transition: { type: "spring", duration: 0.5, stiffness: 100 },
+  },
+};
+
+const textRightVariants = JSON.parse(JSON.stringify(textLeftVariants));
+textRightVariants.hover.x *= -1;
+
+const thumbnailLeftVariants = {
+  rest: {
+    opacity: 0,
+    x: "35%",
+    transition: { type: "spring", duration: 0.5, stiffness: 100 },
+  },
+  hover: {
+    opacity: 0.9,
+    x: "45%",
+    transition: { type: "spring", duration: 0.5, stiffness: 100 },
+  },
+};
+const thumbnailRightVariants = JSON.parse(
+  JSON.stringify(thumbnailLeftVariants)
+);
+thumbnailRightVariants.rest.x = "-50%";
+thumbnailRightVariants.hover.x = "-60%";
 
 const Projects = () => {
   return (
@@ -10,7 +47,7 @@ const Projects = () => {
         </div>
         <div className="projects-showcase-wrapper">
           {projectsData.map((project, i) => (
-            <div
+            <motion.div
               key={"project #" + i}
               className={`project-wrapper ${i % 2 === 0 ? "left" : "right"}`}
               onMouseEnter={() => {
@@ -21,9 +58,26 @@ const Projects = () => {
                   .querySelector(".custom-cursor")
                   .classList.remove("big");
               }}
+              initial="rest"
+              animate="rest"
+              whileHover="hover"
             >
-              <h2>{project.name}</h2>
-            </div>
+              <motion.div
+                className="thumbnail-wrapper"
+                variants={
+                  i % 2 === 0 ? thumbnailLeftVariants : thumbnailRightVariants
+                }
+              >
+                <div className="image-wrapper">
+                  <img src={project.thumbnail} alt="thumbnail" />
+                </div>
+              </motion.div>
+              <motion.h2
+                variants={i % 2 === 0 ? textLeftVariants : textRightVariants}
+              >
+                {project.name}
+              </motion.h2>
+            </motion.div>
           ))}
         </div>
       </div>
