6aa0bddb88a5bcbeac2431990b0e226782688751
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-05 17:41:35 +0200

    Clean up exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as LogoAnimation } from "./LogoAnimation";
 export { default as Navbar } from "./Navbar";
 export { default as Introduction } from "./Introduction";
 export { default as ScrollExploreInfo } from "./ScrollExploreInfo";
+export { default as About } from "./About";
+export { default as Projects } from "./Projects";
