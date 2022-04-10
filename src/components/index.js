b61b430ec7d09c578b2c9d7d710c0b7ec8be0942
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-11 01:34:51 +0200

    Clean up exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Introduction } from "./Introduction";
 export { default as ScrollExploreInfo } from "./ScrollExploreInfo";
 export { default as About } from "./About";
 export { default as Projects } from "./Projects";
+export { default as Contact } from "./Contact";
