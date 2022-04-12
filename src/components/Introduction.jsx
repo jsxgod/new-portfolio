214ddf8952d4f98b95df0f02897cca9cd30065bd
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-12 23:45:11 +0200

    Clean up animations. Add Fade in/out while in view.

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,8 +1,23 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollExploreInfo } from "../components";
 
+const fadeUpVariants = {
+  hide: {
+    opacity: 0,
+    y: "2rem",
+    transition: { duration: 0.6, ease: [0.6, 0.05, -0.01, 0.99] },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.6, ease: [0.6, 0.05, -0.01, 0.99] },
+  },
+};
+
 const Introduction = () => {
+  const [showContent, setShowContent] = useState(false);
+
   const copyEmailInfo = () => {
     navigator.clipboard.writeText("kacpersmyczyk@gmail.com");
     document.querySelector(".custom-cursor").classList.add("copied");
@@ -12,63 +27,63 @@ const Introduction = () => {
     }, 500);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowContent(true);
+    }, 600);
+  }, []);
+
   return (
     <div className="section">
-      <div className="introduction-wrapper">
-        <motion.div
-          initial={{ opacity: 0, y: "2rem" }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.6,
-            ease: [0.6, 0.05, -0.01, 0.99],
-            delay: 0.6,
-          }}
-          className="text-wrapper"
-        >
-          <div className="text-row">
-            <h1>Hi, I'm</h1>
-          </div>
-          <div className="text-row">
-            <h1>
-              <span>Kacper</span>, a software engineer
-            </h1>
-          </div>
-          <div className="text-row">
-            <h1>who likes good design,</h1>
-          </div>
-          <div className="text-row">
-            <h1>
-              coding stuff for the <span>web</span>.
-            </h1>
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, y: "1rem" }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.6,
-            ease: [0.6, 0.05, -0.01, 0.99],
-            delay: 0.9,
-          }}
-          className="email-wrapper"
-          onMouseEnter={() =>
-            document.querySelector(".custom-cursor").classList.add("copy")
-          }
-          onMouseLeave={() =>
-            document.querySelector(".custom-cursor").classList.remove("copy")
-          }
-          onClick={() => copyEmailInfo()}
-        >
-          <h5>kacpersmyczyk@gmail.com</h5>
-        </motion.div>
-      </div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.6 }}
-      >
-        <ScrollExploreInfo />
-      </motion.div>
+      {showContent && (
+        <>
+          <motion.div
+            initial="hide"
+            whileInView="show"
+            viewport={{ amount: 0.5 }}
+            transition={{ staggerChildren: 0.3 }}
+            className="introduction-wrapper"
+          >
+            <motion.div variants={fadeUpVariants} className="text-wrapper">
+              <div className="text-row">
+                <h1>Hi, I'm</h1>
+              </div>
+              <div className="text-row">
+                <h1>
+                  <span>Kacper</span>, a software engineer
+                </h1>
+              </div>
+              <div className="text-row">
+                <h1>who likes good design,</h1>
+              </div>
+              <div className="text-row">
+                <h1>
+                  coding stuff for the <span>web</span>.
+                </h1>
+              </div>
+            </motion.div>
+            <motion.div
+              variants={fadeUpVariants}
+              className="email-wrapper"
+              onMouseEnter={() =>
+                document.querySelector(".custom-cursor").classList.add("copy")
+              }
+              onMouseLeave={() =>
+                document
+                  .querySelector(".custom-cursor")
+                  .classList.remove("copy")
+              }
+              onClick={() => copyEmailInfo()}
+            >
+              <h5>kacpersmyczyk@gmail.com</h5>
+            </motion.div>
+          </motion.div>
+
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <ScrollExploreInfo />
+          </motion.div>
+        </>
+      )}
     </div>
   );
 };
