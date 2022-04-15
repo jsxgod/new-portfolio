55feeca068c74bf811b127448a1fa0deeeac9832
jsxgod <smykoo7@gmail.com>
2022-04-15 23:31:35 +0200

    Create the drawing canvas

---
+++ src/components/DrawingCanvas.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+const DrawingCanvas = ({ color = "black" }) => {
+  const [isDrawing, setIsDrawing] = useState(false);
+  const canvasRef = useRef(null);
+  const canvasContextRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = canvas.offsetWidth * 2;
+    canvas.height = canvas.offsetHeight * 2;
+
+    const canvasContext = canvas.getContext("2d");
+    canvasContext.scale(2, 2);
+    canvasContext.lineCap = "round";
+    canvasContext.lineWidth = 5;
+    canvasContextRef.current = canvasContext;
+  }, []);
+
+  useEffect(() => {
+    canvasContextRef.current.strokeStyle = color;
+  }, [color]);
+
+  const startDrawing = (event) => {
+    const x = event.nativeEvent.offsetX;
+    const y = event.nativeEvent.offsetY;
+    canvasContextRef.current.beginPath();
+    canvasContextRef.current.moveTo(x, y);
+    setIsDrawing(true);
+  };
+
+  const stopDrawing = () => {
+    canvasContextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = (event) => {
+    if (!isDrawing) {
+      return;
+    }
+    const x = event.nativeEvent.offsetX;
+    const y = event.nativeEvent.offsetY;
+    canvasContextRef.current.lineTo(x, y);
+    canvasContextRef.current.stroke();
+  };
+
+  return (
+    <motion.canvas
+      id="drawing-board"
+      ref={canvasRef}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      onMouseDown={(e) => startDrawing(e)}
+      onMouseUp={() => stopDrawing()}
+      onMouseMove={(e) => draw(e)}
+      onMouseEnter={() => {
+        document.querySelector(".custom-cursor").classList.add("brush");
+        document.querySelector(".custom-cursor").style.backgroundColor = color;
+      }}
+      onMouseLeave={() => {
+        document.querySelector(".custom-cursor").classList.remove("brush");
+        document.querySelector(".custom-cursor").style.backgroundColor =
+          "#e2e3ff";
+      }}
+    ></motion.canvas>
+  );
+};
+
+export default DrawingCanvas;
