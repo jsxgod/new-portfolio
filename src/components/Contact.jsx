6fc9f5db9d0748c133c7544f406c1f370fe0e445
jsxgod <smykoo7@gmail.com>
2022-04-15 23:31:23 +0200

    Add doodling functionality. Add color picker.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,45 @@ import PlaneFlight from "../assets/3D/PlaneFlight";
 import { AnimatePresence, motion } from "framer-motion";
 import { LayoutCamera, MotionCanvas } from "framer-motion-3d";
 import FadeInOutWrapper from "./FadeInOutWrapper";
+import { ReactComponent as Crayon } from "../assets/crayon.svg";
+import DrawingCanvas from "./DrawingCanvas";
+
+const CustomLabel = (props) => {
+  return (
+    <label
+      className={`input-wrapper ${props.name}`}
+      onMouseEnter={() => {
+        document.querySelector(".custom-cursor").classList.add("medium");
+      }}
+      onMouseLeave={() => {
+        document.querySelector(".custom-cursor").classList.remove("medium");
+      }}
+      onMouseDown={() => {
+        document.querySelector(".custom-cursor").classList.remove("medium");
+      }}
+    >
+      <span className="input-header">{`${props.name[0].toUpperCase()}${props.name.slice(
+        1
+      )}${props.required ? " *" : ""}`}</span>
+      <input
+        className="input"
+        type="text"
+        value={props.value}
+        required={props.required}
+        spellCheck={false}
+        onChange={(e) => props.handler(e.target.value)}
+      ></input>
+      <span className="border"></span>
+    </label>
+  );
+};
 
 const Contact = () => {
   const [showAnimation, setShowAnimation] = useState(false);
   const [showForm, setShowForm] = useState(false);
+  // 0 - left, 1 - right
+  const [selectedSide, setSelectedSide] = useState(0);
+  const [selectedTool, setSelectedTool] = useState("black");
 
   const [nameValue, setNameValue] = useState("");
   const [surnameValue, setSurnameValue] = useState("");
@@ -18,9 +53,23 @@ const Contact = () => {
     setShowForm(true);
   };
 
-  const handleSubmit = () => {
-    console.log("submit");
+  const switchSide = (n) => {
+    setSelectedSide(n);
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = {
+      name: nameValue,
+      surname: surnameValue,
+      email: emailValue,
+      subject: subjectValue,
+      message: messageValue,
+    };
+    console.log(formData);
+  };
+
+  const handleSaveImage = () => {};
 
   return (
     <motion.div
@@ -33,7 +82,7 @@ const Contact = () => {
       viewport={{ amount: 0.7, once: true }}
     >
       <div className="contact-header-wrapper">
-        <FadeInOutWrapper once={false}>
+        <FadeInOutWrapper once>
           <h2>Contact</h2>
         </FadeInOutWrapper>
       </div>
@@ -51,65 +100,220 @@ const Contact = () => {
             <directionalLight />
           </MotionCanvas>
         ) : (
-          <div className="contact-form-container">
-            <div className="selection-wrapper">standard</div>
-            <div className="selection-wrapper">paper</div>
-            <form className="form-wrapper" onSubmit={() => handleSubmit()}>
-              <label className="input-wrapper name">
-                <span className="input-header">Name *</span>
-                <input
-                  className="input"
-                  type="text"
+          <div
+            className={`contact-form-container ${
+              selectedSide === 0 ? "noise" : "paper"
+            }`}
+          >
+            <div
+              className={`selection-wrapper ${
+                selectedSide === 0 ? "active" : ""
+              }`}
+            >
+              <span
+                onMouseEnter={() => {
+                  document
+                    .querySelector(".custom-cursor")
+                    .classList.add("medium");
+                }}
+                onMouseLeave={() => {
+                  document
+                    .querySelector(".custom-cursor")
+                    .classList.remove("medium");
+                }}
+                onMouseDown={() => {
+                  document
+                    .querySelector(".custom-cursor")
+                    .classList.remove("medium");
+                }}
+                onClick={() => switchSide(0)}
+              >
+                standard
+                {selectedSide === 0 && (
+                  <motion.div
+                    className="underline"
+                    layoutId="underline"
+                  ></motion.div>
+                )}
+              </span>
+            </div>
+            <div
+              className={`selection-wrapper ${
+                selectedSide === 1 ? "active" : ""
+              }`}
+            >
+              <span
+                onMouseEnter={() => {
+                  document
+                    .querySelector(".custom-cursor")
+                    .classList.add("medium");
+                }}
+                onMouseLeave={() => {
+                  document
+                    .querySelector(".custom-cursor")
+                    .classList.remove("medium");
+                }}
+                onMouseDown={() => {
+                  document
+                    .querySelector(".custom-cursor")
+                    .classList.remove("medium");
+                }}
+                onClick={() => switchSide(1)}
+              >
+                paper
+                {selectedSide === 1 && (
+                  <motion.div
+                    className="underline"
+                    layoutId="underline"
+                  ></motion.div>
+                )}
+              </span>
+            </div>
+            {selectedSide === 0 ? (
+              <form
+                id="contact-form"
+                className="form-wrapper"
+                onSubmit={(e) => handleSubmit(e)}
+              >
+                <CustomLabel
+                  name={"name"}
                   value={nameValue}
                   required
-                  spellCheck={false}
-                  onChange={(e) => setNameValue(e.target.value)}
-                ></input>
-              </label>
-              <label className="input-wrapper surname">
-                <span className="input-header">Surname</span>
-                <input
-                  className="input"
-                  type="text"
+                  handler={setNameValue}
+                ></CustomLabel>
+                <CustomLabel
+                  name={"surname"}
                   value={surnameValue}
-                  spellCheck={false}
-                  onChange={(e) => setSurnameValue(e.target.value)}
-                ></input>
-              </label>
-              <label className="input-wrapper email">
-                <span className="input-header">Email *</span>
-                <input
-                  className="input"
-                  type="text"
+                  handler={setSurnameValue}
+                ></CustomLabel>
+                <CustomLabel
+                  name={"email"}
                   value={emailValue}
                   required
-                  spellCheck={false}
-                  onChange={(e) => setEmailValue(e.target.value)}
-                ></input>
-              </label>
-              <label className="input-wrapper subject">
-                <span className="input-header subject">Subject *</span>
-                <input
-                  className="input"
-                  type="text"
+                  handler={setEmailValue}
+                ></CustomLabel>
+                <CustomLabel
+                  name={"subject"}
                   value={subjectValue}
                   required
-                  spellCheck={false}
-                  onChange={(e) => setSubjectValue(e.target.value)}
-                ></input>
-              </label>
-              <label className="input-wrapper message">
-                <span className="input-header subject">Message *</span>
-                <textarea
-                  className="message-input"
-                  value={messageValue}
-                  required
-                  spellCheck={false}
-                  onChange={(e) => setMessageValue(e.target.value)}
-                ></textarea>
-              </label>
-            </form>
-            <div className="send-wrapper">send</div>
+                  handler={setSubjectValue}
+                ></CustomLabel>
+                <label
+                  className="input-wrapper message"
+                  onMouseEnter={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.add("medium");
+                  }}
+                  onMouseLeave={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.remove("medium");
+                  }}
+                  onMouseDown={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.remove("medium");
+                  }}
+                >
+                  <span className="input-header subject">Message *</span>
+                  <textarea
+                    className="message-input"
+                    value={messageValue}
+                    required
+                    spellCheck={false}
+                    onChange={(e) => setMessageValue(e.target.value)}
+                  ></textarea>
+                </label>
+              </form>
+            ) : (
+              <div className="paper-note-wrapper">
+                <AnimatePresence>
+                  <DrawingCanvas color={selectedTool} />
+                </AnimatePresence>
+              </div>
+            )}
+            {selectedSide === 0 ? (
+              <div className="send-wrapper">
+                <input
+                  className="send-button"
+                  type="submit"
+                  value="send"
+                  form="contact-form"
+                  onMouseEnter={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.add("medium");
+                  }}
+                  onMouseLeave={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.remove("medium");
+                  }}
+                  onMouseDown={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.remove("medium");
+                  }}
+                />
+              </div>
+            ) : (
+              <div className="send-wrapper">
+                <button
+                  className="send-button"
+                  onMouseEnter={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.add("medium");
+                  }}
+                  onMouseLeave={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.remove("medium");
+                  }}
+                  onMouseDown={() => {
+                    document
+                      .querySelector(".custom-cursor")
+                      .classList.remove("medium");
+                  }}
+                  onClick={() => handleSaveImage()}
+                >
+                  save
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+
+        {selectedSide === 1 && (
+          <div className="toolbox-wrapper">
+            {[
+              "black",
+              "red",
+              "pink",
+              "orange",
+              "yellow",
+              "lightgreen",
+              "#e1d9d1",
+              "lightblue",
+            ].map((color) => (
+              <motion.div
+                className={`pen-wrapper ${
+                  selectedTool === color ? "active" : ""
+                }`}
+                key={color}
+                animate={selectedTool === color ? { marginRight: 100 } : {}}
+                whileHover={selectedTool === color ? {} : { marginRight: 50 }}
+                onClick={() => {
+                  setSelectedTool(color);
+                }}
+              >
+                <Crayon
+                  fill={color}
+                  stroke={color === "black" ? "white" : ""}
+                />
+              </motion.div>
+            ))}
           </div>
         )}
       </AnimatePresence>
