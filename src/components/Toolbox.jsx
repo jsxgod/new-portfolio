99626ec8a4d2cd7da7b0bd470dbf4cb076712cf0
jsxgod <smykoo7@gmail.com>
2022-04-18 21:05:16 +0200

    Create toolbox component

---
+++ src/components/Toolbox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { ReactComponent as Crayon } from "../assets/svg/crayon.svg";
+
+const Toolbox = ({ selectedColor, handleChangeColor }) => {
+  return (
+    <div className="toolbox-wrapper">
+      {[
+        "black",
+        "#c2cece",
+        "#b72124",
+        "pink",
+        "orange",
+        "yellow",
+        "lightgreen",
+        "lightblue",
+      ].map((color) => (
+        <motion.div
+          className={`pen-wrapper ${selectedColor === color ? "active" : ""}`}
+          key={color}
+          animate={selectedColor === color ? { marginRight: 100 } : {}}
+          whileHover={selectedColor === color ? {} : { marginRight: 50 }}
+          onClick={() => {
+            handleChangeColor(color);
+          }}
+        >
+          <Crayon fill={color} stroke={color === "black" ? "white" : ""} />
+        </motion.div>
+      ))}
+    </div>
+  );
+};
+
+export default Toolbox;
