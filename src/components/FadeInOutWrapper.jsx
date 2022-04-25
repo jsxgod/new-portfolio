c689845b53f316693ec3593418cdadcab2fa875a
Kacper Smyczyk <smykoo7@gmail.com>
2022-04-25 15:30:01 +0200

    Add prop for dynamic variant choice

--- src/components/FadeInOutWrapper.jsx
+++ src/components/FadeInOutWrapper.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { fadeUpVariants } from "../variants/globalVariants";
+import {
+  fadeUpVariants,
+  fadeUpVariantsGentle,
+} from "../variants/globalVariants";
 
-const FadeInOutWrapper = ({ children, once = true, stagger = 0.0 }) => {
+const FadeInOutWrapper = ({
+  children,
+  once = true,
+  stagger = 0.0,
+  gentle = false,
+}) => {
   return (
     <motion.div
       initial="hide"
       whileInView="show"
       exit="hide"
       viewport={{ once: once }}
-      variants={fadeUpVariants}
+      variants={gentle ? fadeUpVariantsGentle : fadeUpVariants}
       transition={{ staggerChildren: stagger }}
       style={{ display: "block" }}
     >
